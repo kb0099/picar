3691a2360cdc93b7f0b021d8f74e3c978f1f1507
Ked B <k.bstkti@gmail.com>
2017-09-05 13:45:07 -0600

    PiCar :: Adds an high level idea to each class files

---
+++ PiCar/image-processor.py
@@ -0,0 +1,20 @@
+
+
+class ImageAnalyzer:
+    "Uses camera to process images"
+    def __init(camera_number):
+        pass;
+
+    def get_turn_direction(imag):
+        " return a range [-a, +a] for left to right turn."
+        pass;
+
+    def process_images(img):
+        # threshold
+        # detect lines
+        # detect obstacles
+        # detect stop signs
+        
+        pass;
+
+    
---
+++ PiCar/obstacle-detector.py
@@ -0,0 +1,13 @@
+
+class ObstacleDetector:
+    "Uses sensors to detect obstacles"
+    def __init__(trigger_pin, echo_pin):
+        pass;
+
+    def get_distance_to_obstacle():
+        pass;
+
+    def is_closer_than(distance):
+        pass;
+
+    
