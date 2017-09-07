c300e34bb263f7cecbd4b034f846f3572246730b
kb0099 <kedar_bas@hotmail.com>
2017-09-07 16:00:00 -0600

    PiCar :: MotionDetector partial implementation

--- PiCar/motion-detector.py
+++ PiCar/motion-detector.py
@@ -8,27 +8,38 @@ import types;
 class MotionDetector(threading.Thread):
   def __init__(self):
     threading.Thread.__init__(self)
-    '''Event to be handled when motion is detected'''
+    '''Callbacks to be handled when motion is detected'''
     self.on_motion_detected_handlers = []
+    self.distance = 0;
+    self.threshold = 0.4;
   
+  '''Each call adds that handler to be called when motion is detected'''
+  def on_motion_detected_handler(self, handler):
+    #self.on_motion_detected_handlers.append(types.MethodType(handler, self));
+    self.on_motion_detected_handlers.append(handler);
+
   def run(self):
     print("run called.");
     while(True):
       time.sleep(1);
-      detected = random.random()
-      if(detected > 0.4):
-        if(self.on_motion_detected):
-          self.on_motion_detected(detected)
-        else:
-          print("event not initialized")
+      self.distance = random.random()
+      if(self.distance > 0.4):
+        for h in self.on_motion_detected_handlers:
+          h(self.distance);
       else:
         print("all good!");
           
-dt = Detector();
+# Sample usage
+dt = MotionDetector();
 print("before calling process_loop");
 dt.start();
 print( "after calling process_loop");
-dt.on_motion_detected = types.MethodType(lambda self, d : print("distance is %f " % d), dt);
+#dt.on_motion_detected_handler(lambda self, d : print("distance is %f " % d));
+dt.on_motion_detected_handler(lambda d : print("distance is %f " % d));
 dt.join();
+
+
+
+
 
 
