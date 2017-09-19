0f228e5e82ee4051300c2cd377e221ce4d6236ca
RockerMAT <rocker.mat@live.com>
2017-09-19 13:21:45 -0600

    Filled some implementation of Powertrain class
    TODO comments list implementation that should get clarification.

--- PiCar/powertrain.py
+++ PiCar/powertrain.py
@@ -1,25 +1,134 @@
+"""Powertrain class to manage two motors along a similar axle.
+"""
 
 class Powertrain:
-    "Uses motors to drive the PiCar"
-    def __init__(left_wheel_forward_pin, left_wheel_backward_pin, right_wheel_forward_pin, right_wheel_backward_pin):
-        self.left_motor = Motor(2, 14, 15);
-        self.right_motor = Motor(3, 4, 19);
+    """Represents a pair of motors. Uses the Motor class to control them.
+    """
 
-    def forward(speed_or_duty_cycle, direction):
+    def __init__(left_wheel_forward, left_wheel_backward,  left_wheel_enable,
+                 right_wheel_forward, right_wheel_backward,
+                 right_wheel_enable):
+        """Constructor.
+
+        Args:
+            left_wheel_forward (int): GPIO pin connected to left motor's
+                forward pin.
+            left_wheel_backward (int): GPIO pin connected to left motor's
+                backward pin.
+            left_wheel_enable (int): GPIO pin connected to left motor's enable
+                pin.
+            right_wheel_forward (int): GPIO pin connected to right motor's
+                forward pin.
+            right_wheel_backward (int): GPIO pin connected to right motor's
+                backward pin.
+            right_wheel_enable (int): GPIO pin connected to right motor's
+                enable pin.
+        """
+        # set motors
+        self.left = Motor(left_wheel_forward, left_wheel_backward,
+                          left_wheel_enable)
+        self.right = Motor(right_wheel_forward, right_wheel_backward,
+                           right_wheel_enable)
+
+    def forward(duty_cycle):
+        """Drive the powertrain forward.
+
+        Args:
+            duty_cycle (float): The duty cycle to run the motors at.
+        """
+        # apply to both motors
+        self.left.forward(duty_cycle)
+        self.right.forward(duty_cycle)
+
+    def reverse(duty_cycle);
+        """Drive the powertrain backward.
+
+        Args:
+            duty_cycle (float): The duty cycle to run the motors at.
+        """
+        # apply to both motors
+        self.left.backward(duty_cycle)
+        self.right.backward(duty_cycle)
+
+    def turn(left_duty_cycle, right_duty_cycle, left_forward=True,
+             right_forward=True):
+        """Drive motor speeds separately to turn.
+
+        Args:
+            left_duty_cyle (float): The duty cyle to run the left motor at.
+            right_duty_cycle (float): The duty cycle to run the right motor at.
+            left_forward (boolean, optional): Flag for the left motor to go
+                forward. Defaults to True.
+            right_forward (boolean, optional): Flag for the right motor to go
+                forward. Defaults to True.
+        """
+        # if-else to use forward/backward
+        if (left_forward) self.left.forward(left_duty_cycle)
+        else self.left.backward(left_duty_cycle)
+
+        if (right_forward) self.right.forward(right_duty_cycle)
+        else self.right.backward(right_duty_cycle)
+
+    def turn_left(max_duty_cycle, intensity):
+        """Drive the motors to turn left.
+
+        Args:
+            max_duty_cycle (float): The duty cycle to run the right motor at.
+                The left motor runs at a portion of this.
+            intensity (int): The intensity to turn left.
+        """
+        # TODO how is intensity used to find left_duty_cycle?
         pass;
 
-    def reverse(speed_or_duty_cycle);
+    def turn_right(max_duty_cycle, intensity):
+        """Drive the motors to turn right.
+
+        Args:
+            max_duty_cycle (float): The duty cycle to run the left motor at.
+                The right motor runs at a portion of this.
+            intensity (int): The intensity to turn right.
+        """
+        # TODO how is intensity used to find right_duty_cycle?
         pass;
 
-    def turn(left_motor_speed, right_motor_speed):
-        pass;
+    def turn_intensity(max_duty_cycle, intensity):
+        """Drive the motors to turn based on intensity and its sign.
 
-    # -1 will be max negative +1 will be max positive . 
-    def turn_right():
-        pass;
+        Args:
+            max_duty_cycle (float): The duty cycle to run the faster motor at.
+                The other motor runs at a portion of this.
+            intensity (int): The intensity to turn. The sign of the intensity
+                affects the turn direction (e.g. negative for left, positive
+                for right).
+        """
+        # TODO define intensity better: limits, how it calculates slower duty
+        #  cycle, behavior on 0; do this also for turn_left() and turn_right()
+        if (intensity < 0) turn_left(max_duty_cycle, -1*intensity)
+        else turn_right(max_duty_cycle, intensity)
 
-    def turn_left():
-        pass;
+    def pivot(duty_cycle, clockwise=False):
+        """Drive the motors to run opposite of each other to pivot
+
+        Args:
+            duty_cycle (float): The duty cycle to run the motors at.
+            clockwise (boolean, optional): Flag for pivoting clockwise.
+                Defaults to False.
+        """
+        if (clockwise) turn(duty_cycle, duty_cycle, right_forward=False)
+        else turn(duty_cycle, duty_cycle, left_forward=False)
 
     def stop():
+        """Stop the motors.
+        """
+        # TODO should this call brake() for a short time then call off()?
         pass;
+
+    def cleanup():
+        """Cleanup GPIO pins for the motors.
+
+        Calling this releases the motors, so further use of the powertrain will
+        not be possible.
+        """
+        # apply to both motors
+        self.left.cleanup()
+        self.right.cleanup()
