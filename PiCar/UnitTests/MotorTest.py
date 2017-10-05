8c59739e3ebf51c0ae81a9331cedf07dc742bfe1
Rocker-Mitchell <rocker.mat94@gmail.com>
2017-10-05 13:42:16 -0600

    Start of motor unit testing
    No runnable implementation included

--- PiCar/UnitTests/MotorTest.py
+++ PiCar/UnitTests/MotorTest.py
@@ -1,7 +1,27 @@
+"""Unit tests for the Motor class
+
+'mock' is available as of Python 3.3
+"""
 #import sys
 #sys.path.insert(0,'..')
 
+# Imports
+import mock
+import unittest
 from .. import motor
-from config import *
 
-m = Motor()
+class TestMotorClass(unittest.TestCase):
+	
+	def test_construct(self):
+		"""Test constructor behaves as expected.
+		"""
+		# check frwd_p, bkwd_p, enbl_p
+		
+		# check GPIO.setup is called right
+		
+		# check GPIO.pwm is called right, returns a test dummy
+		
+		# check pwm
+
+if __name__ == '__main__':
+	unittest.main()
