30903702d721055ab8382cc23c8630e21f3678a3
Rocker-Mitchell <rocker.mat94@gmail.com>
2017-10-09 11:02:59 -0600

    Implements runnable test of Motor constructor
    It passes unit tests currently on my pc on Python 2.7

--- PiCar/UnitTests/MotorTest.py
+++ PiCar/UnitTests/MotorTest.py
@@ -2,26 +2,32 @@
 
 'mock' is available as of Python 3.3
 """
-#import sys
-#sys.path.insert(0,'..')
+# modify system path to access modules above
+import sys
+sys.path.append(sys.path[0] + "\\..")
 
 # Imports
-import mock
 import unittest
-from .. import motor
+#from mock import patch, Mock
+from motor import Motor
 
 class TestMotorClass(unittest.TestCase):
 	
-	def test_construct(self):
+	def testConstruct(self):
 		"""Test constructor behaves as expected.
 		"""
+
+		m = Motor(1, 2, 3)
 		# check frwd_p, bkwd_p, enbl_p
-		
+		self.assertEqual(m.frwd_p, 1)
+		self.assertEqual(m.bkwd_p, 2)
+		self.assertEqual(m.enbl_p, 3)
 		# check GPIO.setup is called right
 		
 		# check GPIO.pwm is called right, returns a test dummy
 		
 		# check pwm
+
 
 if __name__ == '__main__':
 	unittest.main()
