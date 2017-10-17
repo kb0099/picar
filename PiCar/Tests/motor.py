2081fb006882da2b25493cc9c8ef702c0c66aa54
kb0099 <kedar_bas@hotmail.com>
2017-10-16 19:31:44 -0600

    physical tests::adds motor tests

---
+++ PiCar/Tests/motor.py
@@ -0,0 +1,27 @@
+from ..motor import Motor;
+from ..config import *;
+
+
+left = Motor(LFP, LBP, LEP);
+right = Motor(RFP, RBP, REP);
+
+def test_left():
+	left.forward(30);
+	sleep(1);
+	left.off();
+	sleep(1);
+	left.backward(1);
+	sleep(1);
+	left.off();
+
+def test_right():
+	right.forward(30);
+	sleep(1);
+	right.off();
+	sleep(1);
+	right.backward(1);
+	sleep(1);
+	right.off();
+
+test_left();
+test_right();
