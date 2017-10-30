846016fbc435cb28cd9f99ae857025532398fca1
Tim <tmdorny@gmail.com>
2017-10-30 13:09:09 -0600

    image processing compatibility test

---
+++ PiCar/image-processor-test.py
@@ -0,0 +1,31 @@
+from ImageProcessor import ImageProcesor;
+from Powertrain import Powertrain;
+import car_config.py
+
+def start(self):
+    imgpr = ImageProcesor(0, 1)
+    pt = Powertrain(LFP, LBP, LEP, RFP, RBP, REP)
+    # self.image_processor.start();
+    try:
+        pt.forward(100)
+        while(True):
+            status = imgpr.check_status()
+            print("Status: {0}".format(status))
+            pt.turn_intensity(100, status)
+
+    except KeyboardInterrupt:
+        self.stop();
+
+def stop():
+    # probably can call cleanup here
+    # release all resources
+    self.image_processor.cleanup();
+    GPIO.cleanup();
+
+def pause():
+    PowerTrain.stop();
+
+
+# if main function
+#    car_object = PiCar();
+#    car_object.Start(what_type_of_operation);
--- PiCar/image-processor.py
+++ PiCar/image-processor.py
@@ -8,43 +8,54 @@ import datetime
 import RPi.GPIO as GPIO
 
 class ImageProcessor:
-	"""
+	'''
 	A module to be used for fetching images and performing various image processing functions
 	to provide instructions to the vehicle.
 	Attributes:
 		camera_port: An int representing the port of the desire camera.
 		lane_type: An int that tells the class to look for light lines on dark background (1) or dark lines on light background (0)
-	"""
-    def __init__(self, camera_port):
-    	"""
+	'''
+    def __init__(self, camera_port, lane_type):
+    	'''
     	Return a ImageProcessor capturing from the provided camera port.
-    	"""
+    	camera_port: port of desired camera (int)
+    	lane_type: value 1 for light on dark, value 0 for dark on light lanes
+    	'''
+    	self.lane_type = lane_type
     	self.camera = cv2.VideoCapture(camera_port)
-    	self.lane_type = 1
+    	self.init_camera()
         pass;
 
+    def init_camera(self)
+    	'''
+    	Skips a number of frames to allow the webcam to adjust light levels etc.
+    	'''
+    	for i in xrange(30):
+ 			temp = get_image()
+
 	def get_image(self):
-		"""
+		'''
 		Fetches an image from the camera.
-		"""
+		'''
 		retval, im = self.camera.read()
 		return im
 
 	def set_lane_type(self, lane_t):
-		"""
+		'''
 		Sets the line type to light on dark [1] or dark on light [0]
-		"""
+		'''
 		if lane_t > 1 or lane_t < 0:
+			raise ValueError("Lane type must be of value 0 or 1.")
 			return
 		else:
 			self.lane_type = lane_t
     def check_status(self):
-        """
+        '''
         Analyzes an image and provides instructions/parameters to support the vehicle's path.
         Returns a value between -1 and 1 that represents the turning action to be taken.
         A positive value represents a right turn while a negative value represents a left turn.
         The magnitude of the value determines the amount of turning desired (1/-1 being max).
-        """
+        '''
         img = self.get_image()
         # Get the rows, columns, and channel values from the image
 		rows,columns,channels = img.shape
@@ -52,7 +63,7 @@ class ImageProcessor:
 		bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # Process image using Binary Thresholding
 		if lane_type:
-			ret,thresh = cv2.threshold(bw, 235, 255, cv2.THRESH_BINARY)
+			ret,thresh = cv2.threshold(bw, 245, 255, cv2.THRESH_BINARY)
 		else:
 			ret,thresh = cv2.threshold(bw, 100, 255, cv2.THRESH_BINARY_INV)
 		# Initial values
@@ -70,8 +81,6 @@ class ImageProcessor:
 
 		# Detect potential lane markings at the 'low' pixel row value
 		for i in range(0, columns):
-			# Uncomment the below line to draw a line on the row that is being detected:
-			#thresh.itemset((low, i), 155)
 			if(thresh.item(low, i) is 255 and prog is 0):
 				start1_low = i
 				prog = 1
@@ -84,13 +93,12 @@ class ImageProcessor:
 			elif(thresh.item(low, i) is 0 and prog is 3):
 				end2_low = i
 				prog = 4
+			# Uncomment the below line to draw a line on the row that is being detected:
 			thresh.itemset((low, i), 155)
 		prog = 0
 
 		# Detect potential lane markings at the 'high' pixel row value
 		for j in range(0, columns):
-			# Uncomment the below line to draw a line on the row that is being detected:
-			#thresh.itemset((high, j), 155)
 			if(thresh.item(high, j) is 255 and prog is 0):
 				start1_high = j
 				prog = 1
@@ -103,6 +111,7 @@ class ImageProcessor:
 			elif(thresh.item(high, j) is 0 and prog is 3):
 				end2_high = j
 				prog = 4
+			# Uncomment the below line to draw a line on the row that is being detected:
 			thresh.itemset((high, j), 155)
 
 		# Condense the two-point lane detection to a single point at the midpoint of the lane
@@ -123,28 +132,36 @@ class ImageProcessor:
 		else:
 			high_2 = 0
 
+		print("Low l/2: {0} / {1}\nHigh 1/2: {2} / {3}\n".format(low_1, low_2, high_1, high_2))
+		cv2.imwrite('../../../images/' + datetime.datetime.now().isoformat() + ".jpg", thresh)
+		cv2.imwrite('../../../images/' + datetime.datetime.now().isoformat() + "_RAW.jpg", img)
+
 		# If only one lane is detected, determine which direction it is 'slanting' to tell the car which way to go
 		if high_2 == 0 or low_2 == 0:
 			if high_1 != 0 and low_1 != 0:
 				if high_1 > low_1:
 					# TODO: Turn Right
-					return .75
+					return .25
 				elif low_1 > high_1:
 					# TODO: Turn Left
-					return -.75
+					return -.25
 		# Case where lanes are detected on both lines. [Using midpoint for preemptive adjustments]
 		elif high_1 != 0 and high_2 != 0 and low_1 != 0 and low_2 != 0:
 			# Default forward state
 			midpoint = (high_1 + high_2) / 2
 			if midpoint >= (columns / 2) + 50:
 				# TODO: Turn Right
-				return .25
+				return .10
 			elif midpoint < (columns / 2) - 50:
 				# TODO: Turn Left
-				return -.25
+				return -.10
 			else:
 				# TODO: Continue Straight
 				return 0
+		# No lines detected/issues
+		else:
+			# TODO: Error/Misalignment recovery
+			print("RECOVERY METHOD CALLED")
         return
 
     def process_images(img):
@@ -154,5 +171,7 @@ class ImageProcessor:
         # detect stop signs
         
         pass;
+    def cleanup(self)
+    	del(self.camera)
 
     
