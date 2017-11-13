67a55bd8c35bbe35ad5e47fd48061f24bdf0f0ef
Tim <tmdorny@gmail.com>
2017-11-13 13:02:50 -0700

    imgpr test fixes

--- PiCar/Demoes/d_powertrain.py
+++ PiCar/Demoes/d_powertrain.py
@@ -7,38 +7,42 @@ sys.path.append('..')
 
 from powertrain import Powertrain
 from car_config import *
+import RPi.GPIO as GPIO
 
 pwr = Powertrain(LFP, LBP, LEP, RFP, RBP, REP)
-t = 3 # time to delay actions; in seconds
+t = 2 # time to delay actions; in seconds
+try:
+    # roll forward and backward
+    print("calling forward(80)")
+    pwr.forward(100)
+    time.sleep(t);
+    print("calling reverse(80)")
+    pwr.reverse(100)
+    time.sleep(t)
+    pwr.stop()
 
-# roll forward and backward
-print("calling forward(80)")
-pwr.forward(80)
-time.sleep(t);
-print("calling reverse(80)")
-pwr.reverse(80)
-time.sleep(t)
-pwr.stop()
+    # pivot clockwise and counterclockwise
+    print("calling pivot(50)")
+    pwr.pivot(80)
+    time.sleep(t)
+    print("calling pivot(50, clockwise=True)")
+    pwr.pivot(80, True)
+    time.sleep(t)
+    pwr.stop()
 
-# pivot clockwise and counterclockwise
-print("calling pivot(50)")
-pwr.pivot(50)
-time.sleep(t)
-print("calling pivot(50, clockwise=True)")
-pwr.pivot(50, True)
-time.sleep(t)
-pwr.stop()
+    # turn wide
+    print("calling turn_left(50, 20)")
+    pwr.turn_left(100, 40)
+    time.sleep(t)
+    pwr.stop()
 
-# turn wide
-print("calling turn_left(50, 20)")
-pwr.turn_left(50, 20)
-time.sleep(t)
-pwr.stop()
+    # turn tight
+    print("calling turn_right(80, 80)")
+    pwr.turn_right(100, 40)
+    time.sleep(t)
+    pwr.stop()
 
-# turn tight
-print("calling turn_left(80, 80)")
-pwr.turn_left(80, 80)
-time.sleep(t)
-pwr.stop()
-
-print("done")
+    print("done")
+except KeyboardInterrupt:
+    pwr.stop()
+    GPIO.cleanup()
