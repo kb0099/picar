5bcac30b99372ab19744c326d7b9ca9590b14ef7
Tim <tmdorny@gmail.com>
2017-11-13 12:19:27 -0700

    add powertrain test

--- PiCar/test-pt.py
+++ PiCar/test-pt.py
@@ -7,16 +7,10 @@ import RPi.GPIO as GPIO
 try:
     pt = Powertrain(LFP, LBP, LEP, RFP, RBP, REP)
     time.sleep(3)
-    print("Turning Right Intensity .05")
-    pt.turn_intensity(100, .05)
-    time.sleep(3)
-    print("Turning Left Intensity .05")
-    pt.turn_intensity(100, -.05)
-    time.sleep(3)
     print("Turning Right Intensity .30")
     pt.turn_intensity(100, .30)
     time.sleep(3)
-    print("Turning Right Intensity .30")
+    print("Turning Left Intensity .30")
     pt.turn_intensity(100, -.30)
 except KeyboardInterrupt:
     pt.stop()
