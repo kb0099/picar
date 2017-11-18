e13359d6da2fa380eb4793d4ec112b060d3a1bcc
KB <k.bstkti@gmail.com>
2017-11-18 07:06:42 +0000

    powertrain/motor tests

--- PiCar/Tests/pt.py
+++ PiCar/Tests/pt.py
@@ -8,24 +8,51 @@ sys.path.append('..');
 from motor import Motor;
 from car_config import *;
 from powertrain import Powertrain;
+## Note  :
+## Usage : python pt.py <test#> <args>
 
-## Usage : python powtertrain.py <test#> <args>
-def test0(x):
-    print ("ready...", x);
+## Example
+## To run test0 with argument 0
+##      python pt.py 0 0
+##
+## To run test1 with duty cycle of 40:
+##      python pt.py 1 40
+##
+## And similar for other tests!
+
+
+# global
+pt = Powertrain(LFP, LBP, LEP, RFP, RBP, REP);
+
+# test0 is for motors
+def test0(dc):
+    m1 = Motor(LFP, LBP, LEP);
+    m2 = Motor(RFP, RBP, REP);
+    m1.forward(dc[0]);
+    time.sleep(2);
+    m1.off();
+    m2.forward(dc[0]);
+    time.sleep(2);
+    
     
 def test1(dc):
-    pt = Powertrain(LFP, LBP, LEP, RFP, RBP, REP);
-    pt.forward(dc);
+    pt.forward(dc[0]);
+    time.sleep(2);
     
 def test2(dc):
-    pt = Powertrain(LFP, LBP, LEP, RFP, RBP, REP);
-    pt.backward(dc);
+    pt.reverse(dc[0]);
+    time.sleep(2);
+
+    
     
 def test3(x):
     print(x);
     
-# 
+# global tests
 tests = [test0, test1, test2, test3];
 if __name__ == "__main__":
-    tests[int(sys.argv[1])](sys.argv[2])
+    tests[int(sys.argv[1])](map(float, sys.argv[2:]));
+    print("all done!");
+    pt.cleanup();
+    
     
