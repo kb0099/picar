ee3c8ff23e7034ece9dd43f9a52a77bde2643b0c
Tim <tmdorny@gmail.com>
2017-11-30 15:31:52 -0700

    fix whitespace

--- PiCar/image-processor-test.py
+++ PiCar/image-processor-test.py
@@ -16,35 +16,35 @@ def start():
     try:
 	
         #pt.turn_intensity(base_duty_cycle, base_intensity)
-	#time.sleep(2)
-	#pt.turn_intensity(base_duty_cycle, -80)
-	#time.sleep(3)
-	#pt.turn_intensity(base_duty_cycle, 80)
-	#time.sleep(3)
+    	#time.sleep(2)
+    	#pt.turn_intensity(base_duty_cycle, -80)
+    	#time.sleep(3)
+    	#pt.turn_intensity(base_duty_cycle, 80)
+    	#time.sleep(3)
 
-    #populate queue
-    imgs = Queue()
-    imgs.put(imgpr.get_image())
-    imgs.put(imgpr.get_image())
-    imgs.put(imgpr.get_image())
-    imgs.put(imgpr.get_image())
-    imgs.put(imgpr.get_image())
-	lastAdj = 0
-        while(True):
-        img_to_process = imgs.get()
+        #populate queue
+        imgs = Queue()
         imgs.put(imgpr.get_image())
-	    #pt.stop()
-	    #time.sleep(.05)
-	    print("\n\n")
-            adjustment = imgpr.check_status() * 1000
-            print("Adjustment: {0}".format(adjustment))
-            if adjustment == 17 * 1000 or adjustment == 0:
-                print("Unexpected resolution of image processing method.\n")
-		pt.turn_intensity(base_duty_cycle, (base_intensity + lastAdj))
-            else:
-                pt.turn_intensity(base_duty_cycle, (base_intensity + adjustment))
-		lastAdj = adjustment
-	    #time.sleep(.13)
+        imgs.put(imgpr.get_image())
+        imgs.put(imgpr.get_image())
+        imgs.put(imgpr.get_image())
+        imgs.put(imgpr.get_image())
+    	lastAdj = 0
+        while(True):
+            img_to_process = imgs.get()
+            imgs.put(imgpr.get_image())
+    	    #pt.stop()
+    	    #time.sleep(.05)
+    	    print("\n\n")
+                adjustment = imgpr.check_status() * 1000
+                print("Adjustment: {0}".format(adjustment))
+                if adjustment == 17 * 1000 or adjustment == 0:
+                    print("Unexpected resolution of image processing method.\n")
+    		pt.turn_intensity(base_duty_cycle, (base_intensity + lastAdj))
+                else:
+                    pt.turn_intensity(base_duty_cycle, (base_intensity + adjustment))
+    		lastAdj = adjustment
+    	    #time.sleep(.13)
 
 
     except KeyboardInterrupt:
