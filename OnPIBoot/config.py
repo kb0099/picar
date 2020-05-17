e7da696f59d14006a5b1ceee44603e248aed79e3
kb0099 <pra_ked@live.com>
2020-05-16 19:03:51 -0500

    Sample Config file

---
+++ OnPIBoot/config.py
@@ -0,0 +1,11 @@
+'''
+    Internal configurations 
+'''
+appName     = "PI Boot Notifier"
+userName    = "user@gmail.com"
+password    = "userpass"
+recipients  = ["receiver@gmail.com"]
+
+''' Local web server port '''
+PORT        = 2000;
+WEB_ROOT    = "C:\\";
